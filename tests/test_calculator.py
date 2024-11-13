6bb107de8b22efbd09ed4a3be627ff2a2a4414b4
CJE <engels.carlos@gmail.com>
2024-11-12 19:21:33 -0700

    tweak unit test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,6 +2,12 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
+# Add the 'src' directory to sys.path
+base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Get the base directory
+src_path = os.path.join(base_path, 'src')  # Path to the 'src' folder
+sys.path.insert(0, src_path)  # Insert it at the start of sys.path
+
+# Print the sys.path to confirm
 print("sys.path:", sys.path)
 
 import unittest
@@ -14,10 +20,3 @@ class TestCalculator(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-#Test cases from first version:
-# no re roll yet
-#print("4x Melta rifles into Ctan")
-#calculateDamage(4, 9, 3, 11, 4)
-#print("2x Multi-melta into Ctan")
-#calculateDamage(4, 9, 4, 11, 4)
